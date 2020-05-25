9ee1b839642472131927f431e60f4975c04a24b9
wulff <wulff@ratatosk.net>
2020-05-25 09:43:19 +0200

    Add basic JWK support

---
+++ src/JWK/KeySet.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Informeren\OAuth2\Client\JWK;
+
+use RuntimeException;
+
+class KeySet
+{
+
+    /**
+     * @var Key[]
+     */
+    protected $keys;
+
+    public function __construct()
+    {
+        $this->keys = [];
+    }
+
+    public static function fromJson(string $json): KeySet
+    {
+        $data = json_decode($json, true);
+
+        if (empty($data['keys'])) {
+            $message = 'No keys available';
+            throw new RuntimeException($message);
+        }
+
+        $keyset = new self();
+
+        foreach ($data['keys'] as $key) {
+            if ($key['kty'] !== 'RSA') {
+                continue;
+            }
+
+            $rsa = Key::fromState($key);
+
+            $keyset->add($rsa);
+        }
+
+        return $keyset;
+    }
+
+    public function add(Key $key)
+    {
+        $this->keys[] = $key;
+    }
+
+    public function find(string $id): Key
+    {
+        $keys = array_filter($this->keys, function (Key $key) use ($id) {
+            return $key->getId() === $id;
+        });
+
+        if (count($keys) !== 1) {
+            $message = 'Key not found';
+            throw new RuntimeException($message);
+        }
+
+        return reset($keys);
+    }
+}
