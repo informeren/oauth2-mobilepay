fb4ac6ca4b0d4451c69d92a93c3db7616b2975d4
wulff <wulff@ratatosk.net>
2020-05-29 15:31:48 +0200

    Complain if resource owner ID is unavailable

--- src/Provider/MobilePayResourceOwner.php
+++ src/Provider/MobilePayResourceOwner.php
@@ -3,6 +3,7 @@
 namespace Informeren\OAuth2\Client\Provider;
 
 use League\OAuth2\Client\Provider\ResourceOwnerInterface;
+use RuntimeException;
 
 class MobilePayResourceOwner implements ResourceOwnerInterface
 {
@@ -24,6 +25,9 @@ class MobilePayResourceOwner implements ResourceOwnerInterface
      */
     public function getId()
     {
+        if (empty($this->response['merchant_id'])) {
+            throw new RuntimeException('Resource owner ID not available');
+        }
         return $this->response['merchant_id'];
     }
 
